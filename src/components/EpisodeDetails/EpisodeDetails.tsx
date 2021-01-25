e76ba68a61d6c29ea21a98fc4cd321fe35c2773b
soyyogi <raoyogesh78@yahoo.in>
2021-01-25 04:46:32 +0100

    #6 added characters api requests

--- src/components/EpisodeDetails/EpisodeDetails.tsx
+++ src/components/EpisodeDetails/EpisodeDetails.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import CharacterCard from '../CharacterCard/CharacterCard';
 import './EpisodeDetails.css';
 
@@ -7,20 +8,35 @@ interface Props {
 }
 
 function EpisodeDetails({ episode }: Props) {
-    episode.length ? console.log(episode) : console.log('not selected')
+    const [characters, setCharacters] = useState<any>();
+
+    useEffect(() => {
+        (async function renderCharacters() {
+            if (episode[0]?.characters.length > 0) {
+                const response = await axios.all(episode[0]?.characters?.map((epi: string) => axios.get(epi)))
+                setCharacters(response)
+            }
+        })();
+    }, [episode]);
+
+
+    useEffect(() => {
+        console.log(characters)
+    }, [characters])
 
     return (
         <section className="episodeDetails">
             <div className="episodeDetails_header">
-                <h1 className="title">{episode[0]? episode[0]['name'] : ''}</h1>
+                <h1 className="title">{episode[0] ? episode[0]['name'] : ''}</h1>
                 <p>
-                    <span className="air_date">{episode[0]? episode[0]['air_date'] : ''}</span>
-                    <span className="episode_code">{episode[0]? episode[0]['episode'] : ''}</span>
+                    <span className="air_date">{episode[0] ? episode[0]['air_date'] : ''}</span>
+                    <span className="episode_code">{episode[0] ? episode[0]['episode'] : ''}</span>
                 </p>
             </div>
             <div className="episodeDetails_characters">
-                <CharacterCard image="https://rickandmortyapi.com/api/character/avatar/1.jpeg" name="Rick Sanchez" status="Alive" species="Human" />
-                <CharacterCard image="https://rickandmortyapi.com/api/character/avatar/1.jpeg" name="Rick Sanchez Rick Sanchez" status="Alive" species="Human" />
+                {/* <CharacterCard image="https://rickandmortyapi.com/api/character/avatar/1.jpeg" name="Rick Sanchez" status="Alive" species="Human" />
+                <CharacterCard image="https://rickandmortyapi.com/api/character/avatar/1.jpeg" name="Rick Sanchez Rick Sanchez" status="Alive" species="Human" /> */}
+                {/* {renderCharacters()} */}
             </div>
         </section>
     );
