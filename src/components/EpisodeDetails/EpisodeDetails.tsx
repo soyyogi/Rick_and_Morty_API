2264515e2accda32fffb87d66f57d2a45f61c0c4
soyyogi <raoyogesh78@yahoo.in>
2021-01-25 01:56:31 +0100

    #5 added selected episode details

--- src/components/EpisodeDetails/EpisodeDetails.tsx
+++ src/components/EpisodeDetails/EpisodeDetails.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './EpisodeDetails.css';
 
 interface Props {
-    episode: {}[]
+    episode: any
 }
 
 function EpisodeDetails({ episode }: Props) {
@@ -10,6 +10,13 @@ function EpisodeDetails({ episode }: Props) {
 
     return (
         <section className="episodeDetails">
+            <div className="episodeDetails_header">
+                <h1 className="title">{episode[0]? episode[0]['name'] : ''}</h1>
+                <p>
+                    <span className="air_date">{episode[0]? episode[0]['air_date'] : ''}</span>
+                    <span className="episode_code">{episode[0]? episode[0]['episode'] : ''}</span>
+                </p>
+            </div>
         </section>
     );
 }
